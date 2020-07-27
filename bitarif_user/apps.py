e1b6ee159377f6aba61f387ffb08f98cff845de8
bugragoksu <bugragoksu2@gmail.com>
2020-07-27 20:06:41 +0300

    add user model

---
+++ bitarif_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BitarifUserConfig(AppConfig):
+    name = 'bitarif_user'
---
+++ bitarif_user/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class BitarifUser(models.Model):
+    firebase_id = models.CharField(max_length=400, unique=True)
+    email = models.EmailField(('Email Adress'), unique=True)
+    name = models.CharField(max_length=100, blank=True, null=True, default='bitarif User')
+    profile_pic = models.CharField(max_length=500, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    follower = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+
+    def __str__(self):
+        return self.email
